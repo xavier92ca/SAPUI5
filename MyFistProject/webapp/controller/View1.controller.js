ebdac01d27ccd772a4a8880bf07c88a77677cef9
xavier92ca <franciscox92@outlook.com>
2021-03-08 06:13:27 +0000

    Second commit

---
+++ MyFistProject/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.myfirstapp.MyFistProject.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
